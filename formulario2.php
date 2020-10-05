c356fc421bb74968abcd3ac9f7f0483b92408fce
Marmota-hub <bastianjorquera8@gmail.com>
2020-10-05 14:17:53 -0300

    agregue un parrafo en la linea 10

--- formulario2.php
+++ formulario2.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>HOLA ESTA ES LA PRUEBA 2</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque possimus distinctio suscipit eius natus aliquid modi molestiae ratione quod alias, nemo dignissimos non ducimus cum assumenda excepturi animi mollitia quibusdam!</p>
 </body>
 </html>
