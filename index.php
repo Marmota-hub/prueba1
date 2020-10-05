9ad7d65e1c0a188a4ccbf248f5a535e04a83e49e
Marmota-hub <bastianjorquera8@gmail.com>
2020-10-05 11:23:47 -0300

    agregue un parrafo en index

--- index.php
+++ index.php
@@ -6,6 +6,6 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia hic itaque, dolore exercitationem quis alias deserunt sit perspiciatis et aut dolor temporibus facere laudantium quos delectus odio nihil beatae numquam?</p>
 </body>
 </html>
